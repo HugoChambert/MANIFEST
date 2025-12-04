2c9c7f6a23c2f846554107b53c16de07ccb47bd1
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-04 14:45:45 -0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -76,6 +76,11 @@ function App() {
       <header className="app-header">
         <div className="header-content">
           <div className="header-logo-section">
+            <div className="header-logo">
+              <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <path d="M58 28l-22-10-4-12c0-1.1-.9-2-2-2s-2 .9-2 2l-4 12-22 10c-1.1.5-1.6 1.8-1.1 2.9.3.7 1 1.1 1.7 1.1.3 0 .6-.1.9-.2l20-9 2 16-8 4c-.8.4-1.2 1.3-.9 2.1.2.6.8 1 1.4 1 .2 0 .4 0 .6-.1L30 42l12 4c.2.1.4.1.6.1.6 0 1.2-.4 1.4-1 .3-.8-.1-1.7-.9-2.1l-8-4 2-16 20 9c.3.1.6.2.9.2.7 0 1.4-.4 1.7-1.1.5-1.1 0-2.4-1.1-2.9z"/>
+              </svg>
+            </div>
             <h1>Southern Airways</h1>
             <div className="header-badge">Flight Operations</div>
           </div>
